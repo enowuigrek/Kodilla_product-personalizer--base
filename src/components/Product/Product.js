d4660b408cf5948f9a724e7c19887a2048f4833f
Łukasz Nowak <enowuigrek@gmail.com>
2022-05-03 22:06:33 +0200

    add useMemo to getPrice - 12.4

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,6 +1,8 @@
 import styles from './Product.module.scss';
 
 import { useState } from 'react';
+import { useMemo } from 'react';
+
 import ProductImage from '../ProductImage/ProductImage';
 import ProductForm from "../ProductForm/ProductForm";
 
@@ -9,10 +11,10 @@ const Product = props => {
   const [currentColor,setCurrentColor] = useState (props.colors[0]);
   const [currentSize, setCurrentSize] = useState (props.sizes[0].name);
 
-  const getPrice = () => {
+  const getPrice = useMemo (() => {
     const price = props.sizes.find(({ name }) => name === currentSize);
     return props.basePrice + price.additionalPrice;
-  };
+  }, [ props.basePrice, currentSize, props.sizes, ]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,7 +22,7 @@ const Product = props => {
     console.log('Summary');
     console.log('================');
     console.log('Name: ', props.title);
-    console.log('Price: ', getPrice(props.basePrice));
+    console.log('Price: ', getPrice);
     console.log('Size: ', currentSize);
     console.log('Color: ', currentColor);
   };
@@ -32,7 +34,7 @@ const Product = props => {
       <div>
         <header>
           <h2 className={styles.name}> {props.title} </h2>
-          <span className={styles.price}>Price: {getPrice()}$ </span>
+          <span className={styles.price}>Price: {getPrice}$ </span>
         </header>
         <ProductForm
           currentColor={currentColor}
